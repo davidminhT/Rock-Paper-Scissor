883ffbd700ce22a1a899a3e137c32653d9a00f1a
David Truong <davidtruong063@gmail.com>
2024-12-21 20:55:53 -0800

    Complete calculator game.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+let currentRound = 0;
 let humanScore = 0;
 let computerScore = 0;
+let totalRound = 0;
+let gameStarted = false;
 
 function getComputerChoice()
 {
@@ -8,49 +11,95 @@ function getComputerChoice()
     return choiceList[choice];
 };
 
-function getHumanChoice()
-{
-    let userChoice = prompt("Choose Rock, Paper, or Scissor.\n");
-    return userChoice.toLowerCase();
-};
+const StartButton = document.querySelector("#startbutton");
+const RoundInput = document.querySelector("input");
+const GameProgress = document.querySelector("#GameProgress");
+StartButton.addEventListener("click", (e) => {
+    gameStarted = true;
+    GameProgress.textContent = "Game Started!";
+    totalRound = RoundInput.value;
+    RoundInput.value = '';
+    currentRound = 0;
+    humanScore = 0;
+    computerScore = 0;
+    DisplayRoundEvent.textContent = '';
+    DisplayScore.textContent = '';
+});
 
+const buttons = document.querySelector("#button");
+buttons.addEventListener("click", (event) => {
+    let target = event.target;
+    currentRound++;
+    switch(target.id) 
+    {
+        case 'rock':
+            playRound(currentRound, totalRound, target.id);
+            break;
+        case 'paper':
+            playRound(currentRound, totalRound, target.id);
+            break;
+        case 'scissor':
+            playRound(currentRound, totalRound, target.id);
+            break;
+    }  
+    
+});    
 
-function playRound(currentRound)
+const DisplayRoundEvent = document.querySelector('#RoundEvent');
+const DisplayScore = document.querySelector("#DisplayScore");
+function playRound(currentRound, totalRound, humanSelection)
 {
+    if(gameStarted == false)
+    {
+        return
+    }
     var beats = {'rock': 'paper', 'paper': 'scissor', 'scissor': 'rock'};
-
+    GameProgress.textContent = "Round: " + currentRound;
     console.log("Round %d: \n\n", currentRound);
-
-    humanSelection = getHumanChoice();
+    
     computerSelection = getComputerChoice();
 
+    
     if(humanSelection == computerSelection) {
-        console.log("%s against %s! It's a tie!\n");
+        DisplayRoundEvent.textContent = humanSelection + " against " + computerSelection + ". It's a tie...";
+        console.log("%s against %s! It's a tie!\n", humanSelection, computerSelection);
     }
     else if(humanSelection == beats[computerSelection])
     {
+        DisplayRoundEvent.textContent = humanSelection + " against " + computerSelection + ". You won this round!";
         console.log("%s against %s! you win!\n\n", humanSelection, computerSelection);
         humanScore += 1;
     }
     else if(computerSelection == beats[humanSelection])
     {
+        DisplayRoundEvent.textContent = humanSelection + " against " + computerSelection + ". You lost this round!";
         console.log("%s against %s! the comptuter win!\n\n", humanSelection, computerSelection);
         computerScore += 1;
     }
 
+    DisplayScore.textContent = "The current score is Human: " + humanScore + " Computer: " + computerScore;
     console.log("The current score is Human: %d, Computer: %d. \n\n", humanScore, computerScore);
+
+    if(currentRound == totalRound)
+    {
+        determineWinner();
+        return;
+    }
 };
 
-function playGame(numberOfRounds)
-{
-    for(let i = 1; i <= numberOfRounds; i++)
-    {
-        playRound(i);
-    }
-
+function determineWinner()
+{    
+    gameStarted = false;
     if(humanScore > computerScore)
     {
-        console.log("You Win. \n\n");
+        DisplayScore.textContent = "You win the game."
+        console.log("You win the game. \n\n");
     }
-    else{console.log("You Lose. \n\n");}
+    else if(humanScore < computerScore)
+    {
+        DisplayScore.textContent = "You lose the game."
+        console.log("You lose the game. \n\n");
+    }
+    else{DisplayScore.textContent = "Nobody won."}
+
 };
